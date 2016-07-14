1a32794081b601c21a4f2ff48d8e0e35fc40547e
A242 <yorge360@gmail.com>
2016-07-14 10:45:48 -0400

    Music

---
+++ main.js
@@ -0,0 +1,55 @@
+var cNote = document.getElementByID("c_note");
+
+$("#c").click(function(){
+  document.getElementByID("c_note").currentTime = 0;
+  document.getElementByID("c_note").play();
+  cNote.play();
+});
+
+var dNote = document.getElementByID("d_note");
+
+$("#d").click(function(){
+  document.getElementByID("d_note").currentTime = 0;
+  document.getElementByID("d_note").play();
+  dNote.play();
+});
+
+var eNote = document.getElementByID("e_note");
+
+$("#e").click(function(){
+  document.getElementByID("e_note").currentTime = 0;
+  document.getElementByID("e_note").play();
+  eNote.play();
+});
+
+var fNote = document.getElementByID("f_note");
+
+$("#f").click(function(){
+  document.getElementByID("f_note").currentTime = 0;
+  document.getElementByID("f_note").play();
+  fNote.play();
+});
+
+var gNote = document.getElementByID("g_note");
+
+$("#g").click(function(){
+  document.getElementByID("g_note").currentTime = 0;
+  document.getElementByID("g_note").play();
+  gNote.play();
+});
+
+var aNote = document.getElementByID("a_note");
+
+$("#a").click(function(){
+  document.getElementByID("a_note").currentTime = 0;
+  document.getElementByID("a_note").play();
+  aNote.play();
+});
+
+var bNote = document.getElementByID("b_note");
+
+$("#b").click(function(){
+  document.getElementByID("b_note").currentTime = 0;
+  document.getElementByID("b_note").play();
+  bNote.play();
+});
